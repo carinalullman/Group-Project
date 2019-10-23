d46c306366e33b16006263c84c37c3301dad2709
Ben Calvert <benhcalvert@gmail.com>
2019-10-23 17:47:09 -0600

    added MDNs loc preferences function

--- scripts.js
+++ scripts.js
@@ -269,4 +269,71 @@ $(".results-container").empty();
 
 })
 
+// Save user preferences for sharing location
+// CITATION: This function is based on prompting for permission found at
+//https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
+
+function prompt(window, pref, message, callback) {
+  let branch = Components.classes["@mozilla.org/preferences-service;1"]
+                         .getService(Components.interfaces.nsIPrefBranch);
+
+  if (branch.getPrefType(pref) === branch.PREF_STRING) {
+      switch (branch.getCharPref(pref)) {
+      case "always":
+          return callback(true);
+      case "never":
+          return callback(false);
+      }
+  }
+
+  let done = false;
+
+  function remember(value, result) {
+      return function() {
+          done = true;
+          branch.setCharPref(pref, value);
+          callback(result);
+      }
+  }
+
+  let self = window.PopupNotifications.show(
+      window.gBrowser.selectedBrowser,
+      "geolocation",
+      message,
+      "geo-notification-icon",
+      {
+          label: "Share Location",
+          accessKey: "S",
+          callback: function(notification) {
+              done = true;
+              callback(true);
+          }
+      }, [
+          {
+              label: "Always Share",
+              accessKey: "A",
+              callback: remember("always", true)
+          },
+          {
+              label: "Never Share",
+              accessKey: "N",
+              callback: remember("never", false)
+          }
+      ], {
+          eventCallback: function(event) {
+              if (event === "dismissed") {
+                  if (!done) callback(false);
+                  done = true;
+                  window.PopupNotifications.remove(self);
+              }
+          },
+          persistWhileVisible: true
+      });
+}
+
+prompt(window,
+     "extensions.foo-addon.allowGeolocation",
+     "Foo Add-on wants to know your location.",
+     function callback(allowed) { alert(allowed); });
+
 
